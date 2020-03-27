933031e4ce68544947dc579cce3012b699a04e0e
humza3 <humzabutt3@gmail.com>
2020-03-27 00:08:07 +0000

    added post request still need to make it work

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,19 @@
 const cartName = document.getElementById('cart-title');
 const cartImg = document.getElementById('cart-img');
 const cartPrice = document.getElementById('cart-price');
-const cartId = document.getElementById('cart-id');
+const cartSubmit = document.getElementById('cart-submit');
 const cartTitle = document.getElementById('title');
 const cartCard = document.getElementById('card');
 const cartForm = document.getElementById('form_1');
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const addressInput = document.getElementById('address');
+const cityInput = document.getElementById('city');
+const emailInput = document.getElementById('email');
 const x = window.location.hash.substr(1);
 const URL = 'http://localhost:3000/api/cameras/' + x;
+
+
 if (x === "") {
 	console.log(x);
 	cartTitle.textContent = 'There are no items in your cart';
@@ -31,14 +38,43 @@ if (x === "") {
 		cartName.textContent = 'Name Not Found!';
 		cartImg.src = 'images/vcam_1.jpg';	  
 		cartPrice.textContent = 'Price Not Found!';
-		cartId.href = 'confirmation.html';
+		cartSubmit.href = 'confirmation.html';
 	  
 	}
-	const response = JSON.parse(apiRequest.response);	
-		cartName.textContent = response.name; 
-		cartPrice.textContent = "Price: £" + response.price;
-		cartId.href = 'confirmation.html#' + response._id;	
-		cartImg.src = response.imageUrl;	 
-		}
+	let response = JSON.parse(apiRequest.response);	
+	cartName.textContent = response.name; 
+	cartPrice.textContent = "Price: £" + response.price;
+	cartSubmit.href = 'confirmation.html#' + response._id;	
+	cartImg.src = response.imageUrl;	 
+	}
 	};
+}
+
+cartSubmit.addEventListener('click', ($event) => {
+  $event.preventDefault();
+  const post = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+	address: addressInput.value,
+	city: cityInput.value,
+	email: emailInput.value
+  };
+});
+
+function makeRequest(data) {
+  return new Promise((resolve, reject) => {
+    apiRequest.open('POST', URL + '/order');
+    apiRequest.onreadystatechange = () => {
+      if (apiRequest.readyState === 4) {
+        if (apiRequest.status === 201) {
+          resolve(response);
+        } else {
+          reject(response);
+        }
+      }
+    };
+	cartForm.action = 'confirmation.html#' + response._id;
+    apiRequest.setRequestHeader('Content-Type', 'application/json');
+    apiRequest.send(JSON.stringify(data));
+  });
 }
